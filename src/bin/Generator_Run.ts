eeac5a976013ac23b63bb9987da19edf010b7289
Morten Jelle <Zoulember@gmail.com>
2020-10-06 01:46:58 +0200

    Add new arg parser to support new prompt format

--- src/bin/Generator_Run.ts
+++ src/bin/Generator_Run.ts
@@ -1,74 +1,53 @@
 import { prompt } from 'enquirer'
-import { camelCase } from 'change-case'
+import { camelCase, paramCase } from 'change-case'
 import { discoverGenerators } from '~/utils/discover'
 import { getGeneratorJobs } from '~/modules/jobsGenerator'
 import { createHistoryEntry } from '~/modules/history'
 import { executeJobQueue } from '~/modules/executeJobQueue'
-import { GeneratorArgs } from '~/types'
+import { parseGeneratorArgs } from '~/modules/generatorArgsParser/generatorArgsParser'
+import { GeneratorArgs, GeneratorConfig } from '~/types'
 
 const argv = require('yargs-parser')(process.argv.slice(2))
 
 export async function Generator_Run() {
-  const [generatorDirPath, generatorConfig] = await (async () => {
-    const modelNameArg = argv._[0]
-    if (modelNameArg != null) {
-      const paramGenerator = await findGenerator(modelNameArg)
+  // Chose template
+  const templateNameArg = argv._[0]
+  if (templateNameArg != null) {
+    const paramGenerator = await findGenerator(templateNameArg)
 
-      if (paramGenerator != null) return paramGenerator
-      else console.error('Error: Supplied generator was not found, please select one from the list.')
-    }
-    const selectedGeneratorName = await selectGenerator()
-    return (await findGenerator(selectedGeneratorName))!
-  })()
+    if (paramGenerator != null) return paramGenerator
+    else console.error('Error: Supplied generator was not found, please select one from the list.')
+  }
+  const selectedGeneratorName = await selectGenerator()
+  const [generatorDirPath, generatorConfig] = (await findGenerator(selectedGeneratorName))!
   delete argv._
 
-  // Hook
+  // Start
   if (generatorConfig.hooks?.onStart != null) generatorConfig.hooks?.onStart()
 
-  // Hook
+  // Parse args
   if (generatorConfig.hooks?.beforeArgsParser != null) await generatorConfig.hooks.beforeArgsParser(argv)
-
-  let args: GeneratorArgs = {}
-  if (Array.isArray(generatorConfig.args)) {
-    for (const argConfig of generatorConfig.args) {
-      if (
-        argv[argConfig.name] == null ||
-        (typeof argConfig.validate === 'function' && argConfig.validate(argv[argConfig.name], argv))
-      ) {
-        if (argConfig.promptOptions != null) {
-          const answer = await prompt({
-            name: argConfig.name,
-            ...(typeof argConfig.promptOptions === 'function'
-              ? argConfig.promptOptions(args)
-              : argConfig.promptOptions),
-          }) // TODO: Handle error
-          args[argConfig.name] = answer[argConfig.name]
-        }
-        // TODO: Error
-      } else {
-        args[argConfig.name] = argv[argConfig.name]
-      }
-    }
+  let args: GeneratorArgs
+  try {
+    args = await parseGeneratorArgs(generatorConfig, argv)
+  } catch (e) {
+    console.log(e)
+    process.exit(0)
   }
-
-  // Hook
+  const command = getFullCommand(generatorConfig, args)
+  console.log('Running command:', command, '\n')
   if (generatorConfig.hooks?.afterArgsParser != null) args = (await generatorConfig.hooks.afterArgsParser(args)) ?? args
 
-  const command =
-    'kig' +
-    ' ' +
-    generatorConfig.name +
-    ' ' +
-    Object.entries(args)
-      .map(([k, v]) => (typeof v === 'string' && v.includes(' ') ? `--${k} "${v}"` : `--${k} ${v}`))
-      .join(' ')
-
+  // Read the templates and generate the job queue
   const jobs = await getGeneratorJobs(args, generatorDirPath, generatorConfig)
 
-  createHistoryEntry(jobs, command)
+  // Create history entry if the history is enabled
+  await createHistoryEntry(jobs, command)
+
+  // Process the previously created job queue
   executeJobQueue(jobs)
 
-  // Hook
+  // End
   if (generatorConfig.hooks?.onEnd != null) await generatorConfig.hooks?.onEnd()
 }
 
@@ -88,4 +67,18 @@ async function selectGenerator() {
 async function findGenerator(generatorName: string) {
   const availableGenerators = await discoverGenerators()
   return availableGenerators.find(([, gConfig]) => camelCase(gConfig.name) === camelCase(generatorName))
+}
+
+function getFullCommand(generatorConfig: GeneratorConfig, args: GeneratorArgs) {
+  const commandGeneratorName = generatorConfig.name.includes(' ') ? `"${generatorConfig.name}"` : generatorConfig.name
+  const commandArgs = Object.entries(args)
+    .map(([k, v]) => {
+      const argKey = paramCase(k)
+      const argValue = typeof v === 'string' && v.includes(' ') ? `"${v}"` : v
+      return `--${argKey} ${argValue}`
+    })
+    .join(' ')
+  const command = `kig ${commandGeneratorName} ${commandArgs}`
+
+  return command
 }
---
+++ src/modules/generatorArgsParser/generatorArgsParser.ts
@@ -0,0 +1,99 @@
+import { prompt } from 'enquirer'
+import { GeneratorArgConfigEntry, GeneratorArgs, GeneratorArgValue, GeneratorConfig } from '~/types'
+
+export async function parseGeneratorArgs(generatorConfig: GeneratorConfig, argv: { [key: string]: string | boolean }) {
+  let args = {}
+  if (Array.isArray(generatorConfig.args)) {
+    for (let argConfig of generatorConfig.args) {
+      const arg = argv[argConfig.name]
+      if (typeof argConfig === 'function') argConfig = await argConfig(args)
+
+      args[argConfig.name] = parseGeneratorArg(argConfig, arg, args)
+    }
+  }
+
+  return args
+}
+
+async function parseGeneratorArg(argConfig: GeneratorArgConfigEntry, arg: GeneratorArgValue, args: GeneratorArgs) {
+  if (arg == null || (typeof argConfig.validate === 'function' && argConfig.validate(arg, args))) {
+    if (argConfig.prompt != null) {
+      try {
+        let answer: GeneratorArgValue
+        if (typeof argConfig.prompt === 'function') {
+          answer = await argConfig.prompt(args)
+        } else {
+          let promptOptions: Parameters<typeof prompt>[0]
+          switch (argConfig.prompt.type) {
+            case 'confirm':
+              promptOptions = {
+                type: 'confirm',
+                name: argConfig.name,
+                message: argConfig.prompt.message,
+              }
+              break
+
+            case 'input':
+              promptOptions = {
+                type: 'input',
+                name: argConfig.name,
+                message: argConfig.prompt.message,
+              }
+              break
+
+            case 'select':
+              promptOptions = {
+                type: 'autocomplete',
+                name: argConfig.name,
+                message: argConfig.prompt.message,
+                choices: argConfig.prompt.choices,
+              }
+              break
+
+            case 'multi-select':
+              promptOptions = {
+                type: 'autocomplete',
+                name: argConfig.name,
+                muliple: true,
+                message: argConfig.prompt.message,
+                choices: argConfig.prompt.choices,
+              }
+              break
+          }
+
+          const promptResult = await prompt(promptOptions)
+          answer = promptResult[argConfig.name]
+        }
+
+        return parseGeneratorArg(argConfig, answer, args)
+      } catch (e) {
+        throw new ParseGeneratorArgsPromptFailedError(argConfig.name)
+      }
+    }
+    throw new ParseGeneratorArgsMissingArgError(argConfig.name)
+  } else {
+    return arg
+  }
+}
+
+export class ParseGeneratorArgsPromptFailedError extends Error {
+  constructor(argName: string) {
+    super()
+
+    this.argName = argName
+    this.message = `The supplied value for arg: ${argName} is invalid, exiting...`
+  }
+
+  argName: string
+}
+
+export class ParseGeneratorArgsMissingArgError extends Error {
+  constructor(argName: string) {
+    super()
+
+    this.argName = argName
+    this.message = `No value supplied for arg: ${argName}, exiting...`
+  }
+
+  argName: string
+}
